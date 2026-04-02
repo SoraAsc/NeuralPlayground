9bc3f7abc519ebc4e24dbd8841fd319939dc7ae0
SoraAsc <57465032+SoraAsc@users.noreply.github.com>
2026-04-02 00:19:08 -0300

    start of firtst page

---
+++ src/widgets/ui/HeaderItem.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { Brain } from '@lucide/vue'
+</script>
+
+<template>
+  <header class="border-b border-border bg-card/50 backdrop-blur-sm sticky top-0 z-50">
+    <Brain />
+  </header>
+</template>
